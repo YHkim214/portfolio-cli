c6a49ee3c1deca4f66bc22cea4d799a54f3dddfe
YHkim <yoonho12345@naver.com>
2024-01-04 16:57:41 +0900

    회원가입 기능 구현중

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,47 +1,94 @@
-import { Form, Field, ErrorMessage } from "../contexts/FormContext";
+import axios from "axios";
+import { Formik, Form, Field } from "formik";
+import * as Yup from 'yup'
+import { ERROR_CODE_DUP_MEMBER, ERROR_MESSAGE_DUP_MEMBER, JOIN_MEMBER_API } from "../components/api";
 
 function Join() {
+    const JoinSchema = Yup.object().shape({
+        memberName: Yup.string()
+        .required('아이디를 입력해 주세요!'),
+        memberPassword: Yup.string()
+        .required('비밀번호를 입력해 주세요'),
+        memberNickName: Yup.string()
+        .required('닉네임을 입력해 주세요'),
+        memberPasswordVal: Yup.string()
+        .required('비밀번호 확인을 입력해 주세요')
+        .oneOf([Yup.ref('memberPassword'), null], '비밀번호와 동일한 값을 입력해 주세요')
+    })
 
-    const validate = (inputs) => {
-        const errors = {
-            memberName: '',
-            memberPassword: '',
-            memberPasswordVal: ''
-        }
+    const submitHandler = (values, actions) => {
+        const formData = new FormData();
 
-        if(!inputs.memberName) {
-            errors.memberName = "아이디를 입력해주세요!";
-        }
+        Object.entries(values).map(([key, value]) => {
+            formData.append(key, value);
+        })
 
-        if(!inputs.memberPassword) {
-            errors.memberPassword = "패스워드를 입력해주세요!";
-        }
-
-        if(inputs.memberPassword !== inputs.memberPasswordVal) {
-            errors.memberPasswordVal = "비밀번호와 동일한 값을 입력해주세요!";
-        }
-
-        return errors;
+        axios.post(JOIN_MEMBER_API, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            }
+        })
+        .then((response) => {
+            alert('회원가입 완료!');
+        })
+        .catch((error) => {
+            let response = error.response;
+            if(response.data.header.code === ERROR_CODE_DUP_MEMBER) {
+                actions.setFieldError("memberName", ERROR_MESSAGE_DUP_MEMBER);
+            }
+            actions.setSubmitting(false);
+        })
     }
 
     return (
-        <>
-            <Form 
-                _initialValues={{memberName: '',
-                    memberPassword: '',
-                    memberPasswordVal: ''}}
-                _validate={validate}
-                _onSubmit={()=>{}}
+        <div>
+            <h1>회원가입</h1>
+            <Formik
+                initialValues={{
+                    memberName:'',
+                    memberNickName:'',
+                    memberPassword:'',
+                    memberPasswordVal:'',
+                    memberThumbnailFile: undefined
+                }}
+                validationSchema={JoinSchema}
+                onSubmit={submitHandler}
             >
-                <Field type="text" name="memberName" />
-                <ErrorMessage name="memberName" />
-                <Field type="password" name="memberPassword" />
-                <ErrorMessage name="memberPassword" />
-                <Field type="password" name="memberPasswordVal" />
-                <ErrorMessage name="memberPasswordVal" />
-                <button type="submit">회원가입</button>
-            </Form>
-        </>
+                {({errors, touched, isValid, dirty, isSubmitting, setFieldValue}) => (
+                    <Form>
+                        <div>
+                            <label htmlFor="memberName">아이디</label>
+                            <Field id="memberName" name="memberName" type="text" placeholder="아이디를 입력해 주세요"/>
+                            {errors.memberName && touched.memberName && <span>{errors.memberName}</span>}
+                        </div>
+                        <div>
+                            <label htmlFor="memberNickName">닉네임</label>
+                            <Field id="memberNickName" name="memberNickName" type="text" placeholder="닉네임을 입력해 주세요"/>
+                            {errors.memberNickName && touched.memberNickName && <span>{errors.memberNickName}</span>}
+                        </div>
+                        <div>
+                            <label htmlFor="memberPassword">비밀번호</label>
+                            <Field id="memberPassword" name="memberPassword" type="password" placeholder="비밀번호를 입력해 주세요"/>
+                            {errors.memberPassword && touched.memberPassword && <span>{errors.memberPassword}</span>}
+                        </div>
+                        <div>
+                            <label htmlFor="memberPasswordVal">비밀번호 확인</label>
+                            <Field id="memberPasswordVal" name="memberPasswordVal" type="password" placeholder="비밀번호를 한번 더 입력해 주세요"/>
+                            {errors.memberPasswordVal && touched.memberPasswordVal && <span>{errors.memberPasswordVal}</span>}
+                        </div>
+                        <div>
+                        <label htmlFor="memberThumbnail">섬네일</label>
+                            <input id="memberThumbnailFile" name="memberThumbnailFile" onChange={(e) => {
+                                const file = e.target.files[0];
+                                setFieldValue('memberThumbnailFile', file);
+                            }} type="file"/>
+                            {errors.memberThumbnailFile && touched.memberThumbnailFile && <span>{errors.memberThumbnailFile}</span>}
+                        </div>
+                        <button disabled={!(isValid && dirty)} type="submit">회원가입 완료</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     );
 }
 
